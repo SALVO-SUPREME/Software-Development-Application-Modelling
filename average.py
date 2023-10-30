b9374837095cca9c01e07c1355103a72c235c21b
Salvatore Di Salvo <148922448+SALVO-SUPREME@users.noreply.github.com>
2023-10-30 17:38:13 +0000

    Average Calculator

---
+++ average.py
@@ -0,0 +1,10 @@
+# average
+
+num1 = int(input("Enter first integer: "))
+num2 = int(input("Enter second integer: "))
+num3 = int(input("Enter third integer: "))
+num4 = int(input("Enter fourth integer: "))
+
+average = (num1+num2+num3+num4)/4
+
+print("Average is: {}".format(average))
