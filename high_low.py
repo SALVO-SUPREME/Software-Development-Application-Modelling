b17bdd607479cdc13ac25a694dd745807fe90d8c
Salvatore Di Salvo <148922448+SALVO-SUPREME@users.noreply.github.com>
2023-10-30 17:37:47 +0000

    Higher, or Lower Game

---
+++ high_low.py
@@ -0,0 +1,44 @@
+import random
+
+number = random.randint(1, 13)
+
+if number == 1:
+    print("Ace")
+
+elif number == 2:
+    print("2")
+
+elif number == 3:
+    print("3")
+
+elif number == 4:
+    print("4")
+
+elif number == 5:
+    print("5")
+
+elif number == 6:
+    print("6")
+
+elif number == 7:
+    print("7")
+
+elif number == 8:
+    print("8")
+
+elif number == 9:
+    print("9")
+
+elif number == 10:
+    print("10")
+
+elif number == 11:
+    print("Jack")
+
+elif number == 12:
+    print("Queen")
+
+else:
+    print("King")
+
+
